38d56cfa2c2283e0fe18a562b202b547c82bdb44
pradip <pradip@pradips-Mac.local>
2015-01-27 14:51:31 +0530

    First Commit

--- TableAccessories/ViewController.m
+++ TableAccessories/ViewController.m
@@ -8,22 +8,74 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static NSString *MyCellIdentifier = @"SimpleCell";
 
+@interface ViewController () <UITableViewDataSource>
+@property (nonatomic, strong) UITableView *myTableView;
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (void)                        tableView:(UITableView *)tableView
+ accessoryButtonTappedForRowWithIndexPath:(NSIndexPath *)indexPath{
+    
+    /* Do something when the accessory button is tapped */
+    NSLog(@"Accessory button is tapped for cell at index path = %@", indexPath);
+    
+    UITableViewCell *ownerCell = [tableView cellForRowAtIndexPath:indexPath];
+    
+    NSLog(@"Cell Title = %@", ownerCell.textLabel.text);
+    
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    
+    UITableViewCell* result = nil;
+    
+    if ([tableView isEqual:self.myTableView]){
+        
+        result = [tableView
+                  dequeueReusableCellWithIdentifier:MyCellIdentifier
+                  forIndexPath:indexPath];
+        
+        result.textLabel.text =
+        [NSString stringWithFormat:@"Section %ld, Cell %ld",
+         (long)indexPath.section,
+         (long)indexPath.row];
+        
+        result.accessoryType = UITableViewCellAccessoryDetailDisclosureButton;
+        
+    }
+    
+    return result;
+    
 }
+
+- (NSInteger) tableView:(UITableView *)tableView
+  numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+
+- (void)viewDidLoad{
+    [super viewDidLoad];
+    
+    self.myTableView = [[UITableView alloc]
+                        initWithFrame:self.view.bounds
+                        style:UITableViewStylePlain];
+    
+    [self.myTableView registerClass:[UITableViewCell class]
+             forCellReuseIdentifier:MyCellIdentifier];
+    
+    self.myTableView.dataSource = self;
+    
+    self.myTableView.autoresizingMask =
+    UIViewAutoresizingFlexibleWidth |
+    UIViewAutoresizingFlexibleHeight;
+    
+    [self.view addSubview:self.myTableView];
+    
+}
+
 
 @end
